b21c8b5ebf48c8e7e16d1f71a0a48824a48cadc6
Ankit Jha <ankitjha1516@gmail.com>
2026-06-15 12:50:06 +0000

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,34 +1,22 @@
 package com.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    
 
-public class App {
-    public static void main(String[] args) {
-
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        WebDriver driver = new ChromeDriver(options);
-
+        WebDriver driver=new ChromeDriver();
         driver.get("https://www.saucedemo.com");
-
-        driver.findElement(By.id("user-name"))
-              .sendKeys("standard_user");
-
-        driver.findElement(By.id("password"))
-              .sendKeys("secret_sauce");
-
-        driver.findElement(By.id("login-button"))
-              .click();
-
-        System.out.println(driver.getTitle());
-
-        driver.quit();
+        driver.manage().window().maximize();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
     }
 }
